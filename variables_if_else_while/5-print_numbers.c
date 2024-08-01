38b57076a101bad5ff7298ec04cd45ed75d321bd
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-02 01:04:37 +0400

    zero to nine

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - print digit number.
- * 
- * Return: Always 0 (success)
+ * main - Prints digit number.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	int i;
 
-	for (i = 0; i < 10; i++);
-	printf("%d", i);
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
 	putchar('\n');
 	return (0);
 }
