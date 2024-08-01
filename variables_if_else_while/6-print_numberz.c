04022c08b319ed0294c0af5b17e82895d4d33c6b
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-02 01:12:24 +0400

    zero to nine digit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print digit number with putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++);
+	putchar(i);
+	return (0);
+}
