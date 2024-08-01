fc8b4a612a080060bd09d4c84612f7c5bd1b7e82
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-02 00:57:04 +0400

    zero to ten

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print digit number.
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{	
+	int i;
+
+	for (i = 0; i < 10; i++);
+	printf("%d\n", i);
+	return (0);
+}
