ec49ac0531a94a7026be8634be4837cbad26c746
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-02 01:35:06 +0400

    digit

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Print digit number with putchar.
+ * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-	for (i = 48; i < 58; i++);
-	}
-	{	
 		putchar(i);
-	}	
+	}
 	putchar('\n');
 	return (0);
 }
