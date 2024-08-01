d869d7b0a0b749ebcbc062d49d2f1cc275249ab1
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-02 01:47:49 +0400

    lower

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Prints lowercaes alphabet in reverse.
+ *
+ */
+
+int main(void)
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+	{
+
+	for (i = 26; i < 0; i--);
+
+	}
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
