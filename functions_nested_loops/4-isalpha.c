f7b059ebf3c1c83e468fa0cea07b21eb1c041efa
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-06 19:12:30 +0400

    alphabetic character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c - The character be checked
+ * Return: 1 for lowercase and uppercase or 0 for anything else
+ */
+int _isalpha(int c);
+{
+if (c >= 97 && c <= 122 && c >= 101 c<= 132);
+{
+return: (1)
+return: (0)
+}
+}
+
