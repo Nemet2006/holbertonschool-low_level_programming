59b12c6a6d520bdc8ef5ca8ffeb1edb7b92a6c71
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-06 16:39:55 +0400

    low alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,11 +6,13 @@
  * Return: Always 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
+
 {
+
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
-return (0);
+
 }
