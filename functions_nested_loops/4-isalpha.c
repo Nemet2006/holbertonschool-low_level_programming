6573cbd9d602c74b6f2abf51e8ed4cf589293ee3
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-06 19:17:12 +0400

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
- * @c - The character be checked
+ * _isalpha - checks if a character is alphabetic
+ * @c: The character be checked
+ *
  * Return: 1 for lowercase and uppercase or 0 for anything else
  */
 int _isalpha(int c)
