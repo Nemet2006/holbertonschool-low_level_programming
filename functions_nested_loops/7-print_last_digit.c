acafc59d6267487461b2e1b9b3881ebec31cf7cd
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-08 14:22:08 +0400

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,9 @@
  *
  * Return: Always 0.
  */
-int print_last_digit(int n)
+int print_last_digit(int 98)
 {
-	int last_digit = n % 10;
+	int last_digit = 98 % 10;
 	{
 if (last_digit < 0)
 	    last_digit *= -1;
