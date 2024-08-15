ad1c98ed5c1682ff0d9b37d7778c25a352fc3344
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-15 10:56:56 +0400

    number

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers 0 to 9.
+ */
+int _putchar(char c);
+char c;
+
+void print_number(void)
+{
+	for (char c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
