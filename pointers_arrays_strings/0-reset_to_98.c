4f12845f467d8f66d8954fe4af7b3bb037579866
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-26 15:31:22 +0000

    int 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - that takes a pointer to an int as parameter and updates 
+ *
+ * @n: int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
