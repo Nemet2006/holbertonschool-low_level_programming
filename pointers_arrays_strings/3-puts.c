3c24091346772eef66acd9ad93f7fdb47af26c57
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-08-27 13:02:29 +0400

    str

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: print string
+ */
+
+void _puts(char *str);
+{
+	while(*str);
+
+	_putchar(*str++);
+
+	_putchar('\n');
+}
