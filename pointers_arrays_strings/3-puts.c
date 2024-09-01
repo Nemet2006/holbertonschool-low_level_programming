d2dc7d71982abec92ee2301901bc6488c2c01142
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-09-01 05:57:28 +0000

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _puts - prints a string followed by a new line
- * @str: print string
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
  */
-
-void _puts(char *str);
+void _puts(char *str)
 {
-	while(*str);
+while (*str)
+_putchar(*str++);
 
-	_putchar(*str++);
-
-	_putchar('\n');
+_putchar('\n');
 }
